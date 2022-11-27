00949c46b270e1307ff51402e3f909bbaed0ddb1
pabile <1162412+pabile@users.noreply.github.com>
2022-11-27 12:26:12 +0800

    config files for p20v7

---
+++ qmk_firmware/keyboards/pabile/p20/ver2/keymaps/p20v7/config.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#undef PRODUCT
+#define PRODUCT P20v7 Macro Pad
+
+#undef DIODE_DIRECTION
+#define DIODE_DIRECTION ROW2COL
+
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0xA1, 0x3C, 0x27, 0x42, 0x2F, 0x08, 0x96, 0xE2}
+#define MIDI_ADVANCED
+
+#define RGB_DI_PIN B6 
+#define RGBLED_NUM 16
+#define RGBLIGHT_ANIMATIONS 
+#define RGBLIGHT_LED_MAP { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 }
+// #define BACKLIGHT_LEVEL_VAL 150
+#define RGBLIGHT_LIMIT_VAL 80
