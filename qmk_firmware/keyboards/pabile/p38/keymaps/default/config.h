3f7a63ff51a7464eb3c14e6c04c43063b2548cd3
pabile <1162412+pabile@users.noreply.github.com>
2023-02-14 22:02:10 +0800

    Update config.h

--- qmk_firmware/keyboards/pabile/p38/keymaps/default/config.h
+++ qmk_firmware/keyboards/pabile/p38/keymaps/default/config.h
@@ -5,7 +5,7 @@
 // python3 util/vial_generate_keyboard_uid.py
 #define VIAL_KEYBOARD_UID {0x01, 0x65, 0x44, 0x91, 0x8A, 0xC9, 0x6A, 0xD1}
 
-#undef FORCE_NKRO
-#define FORCE_NKRO
-
 #define MIDI_ADVANCED
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
