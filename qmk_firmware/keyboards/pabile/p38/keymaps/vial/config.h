aabbd30b70d67177fea7a9c930e0eddc30e44df5
pabile <1162412+pabile@users.noreply.github.com>
2023-02-16 10:30:05 +0800

    wip vial configurations

---
+++ qmk_firmware/keyboards/pabile/p38/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// 5. Generate and add unique keyboard ID
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0x01, 0x65, 0x44, 0x91, 0x8A, 0xC9, 0x6A, 0xD1}
+
+#define MIDI_ADVANCED
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
