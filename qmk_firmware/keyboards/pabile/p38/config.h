6d339b616c304ec202ebc27f8e1bed3b7f4cd289
pabile <1162412+pabile@users.noreply.github.com>
2023-02-16 11:54:20 +0800

    Update config.h

--- qmk_firmware/keyboards/pabile/p38/config.h
+++ qmk_firmware/keyboards/pabile/p38/config.h
@@ -41,12 +41,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP16
 
-#ifdef RGB_MATRIX_ENABLE
-
 #define RGB_DI_PIN GP28
 #define RGBLED_NUM 36
 #define RGB_MATRIX_LED_COUNT 36
 #define WS2812_PIO_USE_PIO1
+
+#ifdef RGB_MATRIX_ENABLE
 
 /* Enable Framebuffer and keypress effects */
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
