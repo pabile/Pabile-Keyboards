25d9d09b60a795c61bf5402093597d745119ff32
pabile <1162412+pabile@users.noreply.github.com>
2023-03-05 20:32:21 +0800

    fixed rgb matrix

--- qmk_firmware/keyboards/pabile/p20v8/keymaps/default/config.h
+++ qmk_firmware/keyboards/pabile/p20v8/keymaps/default/config.h
@@ -5,8 +5,5 @@
 // python3 util/vial_generate_keyboard_uid.py
 #define VIAL_KEYBOARD_UID {0x79, 0x10, 0x0A, 0x34, 0x31, 0x28, 0xEF, 0x78}
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8
-#define DYNAMIC_KEYMAP_MACRO_COUNT 32
-
 #define MIDI_ADVANCED
 
--- qmk_firmware/keyboards/pabile/p20v8/readme.md
+++ qmk_firmware/keyboards/pabile/p20v8/readme.md
@@ -1,3 +1,1 @@
 #P20v8 Macropad
-
-Powered by RP2040 Pico
