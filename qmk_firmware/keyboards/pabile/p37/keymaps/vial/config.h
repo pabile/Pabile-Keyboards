ba67196387420f4c1b7d4cf38bdee8c4c276843e
pabile <1162412+pabile@users.noreply.github.com>
2023-03-09 20:00:59 +0800

    Add files via upload

---
+++ qmk_firmware/keyboards/pabile/p37/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// 5. Generate and add unique keyboard ID
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0x09, 0xF5, 0x43, 0x0B, 0xC6, 0xF3, 0x57, 0x5D}
+
+#define MIDI_ADVANCED
