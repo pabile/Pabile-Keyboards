9907c45d799335da818ffef4a2cff4157b99a937
pabile <1162412+pabile@users.noreply.github.com>
2023-07-18 21:22:07 +0800

    wip
    need to address last 3 leds

---
+++ qmk_firmware/keyboards/pabile/p42v6/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0xD7, 0xF6, 0x10, 0xC8, 0x9D, 0xB7, 0xE2, 0xB4}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define MIDI_ADVANCED
+
+#define LED_CAPS_LOCK_PIN GP25
