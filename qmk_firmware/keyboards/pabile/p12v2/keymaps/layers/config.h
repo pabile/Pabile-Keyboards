75512524773a72e4d237663f015a53209aeef593
pabile <1162412+pabile@users.noreply.github.com>
2024-06-30 20:52:52 +0800

    Add files via upload

---
+++ qmk_firmware/keyboards/pabile/p12v2/keymaps/layers/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0xCA, 0xAF, 0xA3, 0xFC, 0x9A, 0xEE, 0x12, 0x73}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+
+#define MIDI_ADVANCED
+
+#define LED_NUM_LOCK_PIN GP25
