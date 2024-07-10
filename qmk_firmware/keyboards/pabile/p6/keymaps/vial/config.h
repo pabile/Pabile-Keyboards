e63e91a59de1fdc18e19c994aa32f2adb790d93c
pabile <1162412+pabile@users.noreply.github.com>
2024-07-10 20:54:47 +0800

    Delete qmk_firmware/keyboards/pabile/p6 directory

--- qmk_firmware/keyboards/pabile/p6/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x41, 0x16, 0xDB, 0xD0, 0x53, 0x0A, 0x46, 0xF5}
-// #define MIDI_ADVANCED
-#define USB_SUSPEND_WAKEUP_DELAY 200
