8fe333e443abed21bb52ff35899f877976106001
pabile <1162412+pabile@users.noreply.github.com>
2024-07-16 21:07:19 +0800

    config for rp2040

---
+++ qmk_firmware/keyboards/pabile/p9/ver2/config.h
@@ -0,0 +1,109 @@
+/*
+Copyright 2021 Pabile
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x8085
+#define PRODUCT_ID      0x8009
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    Pabile
+#define PRODUCT         P9v2 Macro Pad
+
+/* key matrix size */
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 3
+
+/* pin-out */
+#define MATRIX_ROW_PINS { C6, D3, D7 }
+#define MATRIX_COL_PINS { B2, D0, D4 }
+#define UNUSED_PINS
+ 
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* encoders */
+#define ENCODERS 3
+
+#define ENCODERS_PAD_B { F7, B1, B3 }
+#define ENCODERS_PAD_A { F4, F5, F6 }
+
+// flip encoder for tall
+// #define ENCODER_DIRECTION_FLIP
+
+#ifdef RGBLIGHT_ENABLE
+#define RGB_DI_PIN B6
+#define RGBLED_NUM 9
+#define RGBLIGHT_SLEEP 
+#define RGBLIGHT_HUE_STEP 8 
+#define RGBLIGHT_SAT_STEP 8 
+#define RGBLIGHT_VAL_STEP 8 
+#define RGBLIGHT_LIMIT_VAL 250 
+#define RGBLIGHT_ANIMATIONS 
+#define RGBLIGHT_LED_MAP { 0,1,2,3,4,5,6,7,8,9 }
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#define DRIVER_LED_TOTAL 9
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_BAND_SAT
+#define ENABLE_RGB_MATRIX_BAND_VAL
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#define ENABLE_RGB_MATRIX_CYCLE_ALL
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#define ENABLE_RGB_MATRIX_DUAL_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#define ENABLE_RGB_MATRIX_RAINDROPS
+#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define ENABLE_RGB_MATRIX_HUE_BREATHING
+#define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#define ENABLE_RGB_MATRIX_HUE_WAVE
+#define ENABLE_RGB_MATRIX_PIXEL_RAIN
+#define ENABLE_RGB_MATRIX_PIXEL_FLOW
+#define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#define ENABLE_RGB_MATRIX_SPLASH
+#define ENABLE_RGB_MATRIX_MULTISPLASH
+#define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
---
+++ qmk_firmware/keyboards/pabile/p9/ver2/keymaps/rp2040/config.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x26, 0xB8, 0xCF, 0xD9, 0x10, 0xB7, 0x2F}
+#define MIDI_ADVANCED
+
+#define USB_SUSPEND_WAKEUP_DELAY 200
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+
+#define MIDI_ADVANCED
+
+/* pin-out RP2040 */
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#define MATRIX_ROW_PINS { GP5, GP0, GP6 }
+#define MATRIX_COL_PINS { GP23, GP3, GP4 }
+#undef ENCODERS_PAD_A
+#undef ENCODERS_PAD_B
+#define ENCODERS_PAD_B { GP26, GP22, GP20 }
+#define ENCODERS_PAD_A { GP29, GP28, GP27 }
+#ifdef RGBLIGHT_ENABLE
+#undef RGB_DI_PIN
+#define RGB_DI_PIN GP21
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+#undef RGB_DI_PIN
+#define RGB_DI_PIN GP21
+#endif
