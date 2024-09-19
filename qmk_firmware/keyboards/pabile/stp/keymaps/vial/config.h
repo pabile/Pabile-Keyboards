c82306c6181ab2e7aaf6b73db97c713af96e3ef9
pabile <1162412+pabile@users.noreply.github.com>
2024-09-19 20:11:53 +0800

    Add files via upload

---
+++ qmk_firmware/keyboards/pabile/stp/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+// python3 util/vial_generate_keyboard_uid.py
+#define VIAL_KEYBOARD_UID {0xF2, 0xAB, 0xCF, 0x32, 0xB3, 0xA3, 0x39, 0xA7}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define MIDI_ADVANCED
+#define USB_SUSPEND_WAKEUP_DELAY 200
