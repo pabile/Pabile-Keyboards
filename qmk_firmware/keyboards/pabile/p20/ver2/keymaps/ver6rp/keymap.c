03ae4287d855097bb58747494bf3f2b400d7e631
pabile <1162412+pabile@users.noreply.github.com>
2025-08-17 18:35:24 +0800

    Add files via upload

--- qmk_firmware/keyboards/pabile/p20/ver2/keymaps/ver6rp/keymap.c
+++ qmk_firmware/keyboards/pabile/p20/ver2/keymaps/ver6rp/keymap.c
@@ -16,13 +16,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
+
 enum custom_keycodes {
-    DBLZERO = USER00,
-    XCOPY,
-    XPASTE,
-    XCUT,
-    CREDITS,
-    CONTACT
+    DBLZERO = SAFE_RANGE,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -33,46 +29,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_code(KC_P0);
         }
         break;
-    case XCOPY:
-        if (record->event.pressed) {
-            tap_code16(C(KC_C));
-        }
-        break;
-    case XPASTE:
-        if (record->event.pressed) {
-            tap_code16(C(KC_V));
-        }
-        break;
-    case XCUT:
-        if (record->event.pressed) {
-            tap_code16(C(KC_X));
-        }
-        break;
-    case CREDITS:
-        if (record->event.pressed) {
-           SEND_STRING("QMK and VIAL are the best things ever! Know more about this keypad at github.com/pabile/Pabile-Keyboards");
-        }
-        break;
-    case CONTACT:
-        if (record->event.pressed) {
-           SEND_STRING("pabile@gmail.com");
-        }
-        break;
     }
     return true;
 };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x4(
-        KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, 
         KC_P7,   KC_P8,   KC_P9,   KC_TAB, 
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
         KC_P1,   KC_P2,   KC_P3,   KC_DEL, 
-        KC_P0,   KC_P0, KC_PDOT, KC_PENT)
+        KC_P0,   DBLZERO, KC_PDOT, KC_PENT)
 };
-
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
-};
-#endif
-
